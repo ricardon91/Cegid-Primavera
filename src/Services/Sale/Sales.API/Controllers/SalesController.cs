5ae8954d30eaf36ac87781d7f665fa610224f601
Ricardo Noronha <ricardo_l.h.p@hotmail.com>
2023-12-22 18:57:42 +0000

    Sales done

---
+++ src/Services/Sale/Sales.API/Controllers/SalesController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Sales.Application.Features.Sales.Commands.DeleteSales;
+using Sales.Application.Features.Sales.Commands.InsertSales;
+using Sales.Application.Features.Sales.Commands.UpdateSales;
+using Sales.Application.Features.Sales.Queries;
+using System.Net;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SalesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SalesController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet(Name = "GetSales")]
+        [ProducesResponseType(typeof(IEnumerable<SalesItemsVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<SalesItemsVm>>> GetSales()
+        {
+            var query = new GetSalesQuery();
+            var sales = await _mediator.Send(query);
+            return Ok(sales);
+        }
+
+        [HttpGet("{id}", Name = "GetSaleById")]
+        [ProducesResponseType(typeof(SalesItemsVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<SalesItemsVm>> GetSaleById(int id)
+        {
+            var query = new GetSaleByIdQuery(id);
+            var sale = await _mediator.Send(query);
+            return Ok(sale);
+        }
+
+        [HttpPost(Name = "InsertSale")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> InsertSale([FromBody] InsertSalesCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut(Name = "UpdateSale")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateSale([FromBody] UpdateSalesCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteSale")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteSale(int id)
+        {
+            var command = new DeleteSalesCommand() { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ src/Services/Sale/Sales.API/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.OpenApi.Models;
+using Sales.API.Extensions;
+using Sales.Application;
+using Sales.Infrastructure;
+using Sales.Infrastructure.Persistance;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddAplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+//General config
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sales.API", Version = "v1" });
+});
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sales.API v1"));
+}
+
+//Migrate SQL Server database
+app.MigrateDatabase<SalesContext>((context, services) =>
+{
+    var logger = services.GetService<ILogger<SalesContextSeed>>();
+    SalesContextSeed.SeedAsync(context, logger).Wait();
+});
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
